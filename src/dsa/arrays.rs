8147751eb67ba6f9d72aabc2ad316f9673ca1d69
Camilo <Camiloandres9704@hotmail.com>
2023-11-09 16:22:38 -0800

    is permutation exercise

--- src/dsa/arrays.rs
+++ src/dsa/arrays.rs
@@ -6,25 +6,47 @@ pub fn has_unique_characters(s: &str) -> bool {
     for c in s.chars() {
         let ascii_val = c as usize;
         if charset[ascii_val] {
-            return false
+            return false;
         }
         charset[ascii_val] = true;
     }
     true
 }
 
+// Check permutations
+// Given two strings decide if one is a permutation of the other
+pub fn is_permutation(m: &str, n: &str) -> bool {
+    // This solution only works if m.len() == n.len()
+    let mut m = Vec::from(m);
+    let mut n = Vec::from(n);
+    // O(m log m)
+    m.sort();
+    // O(n log n)
+    n.sort();
+    m == n
+}
+
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
-    fn test_has_unique_characters(){
-        let strings = [
-            ("abcd", true), 
-            ("abcda", false)
-        ];
+    fn test_has_unique_characters() {
+        let strings = [("abcd", true), ("abcda", false)];
         for (string, result) in strings {
             assert_eq!(has_unique_characters(string), result)
+        }
+    }
+
+    #[test]
+    fn test_is_permutation() {
+        let strings = [
+            ("abcd", "cbad", true), 
+            ("test", "stet", true),
+            ("car", "bad", false),
+        ];
+        for (s1, s2, result) in strings {
+            assert_eq!(is_permutation(s1, s2), result)
         }
     }
 }
